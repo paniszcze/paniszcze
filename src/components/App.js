2aa99d42a8a421e6aa55f597858ee28b765b5b9f
danny <bahuko@gmail.com>
2022-03-09 13:12:22 +0100

    clean react app template

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>paniszcze | portfolio</h1>
+      </header>
+    </div>
+  );
+}
