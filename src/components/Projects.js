cf29f7d1b0f2946f863309eb40c51c3dd5280695
danny <bahuko@gmail.com>
2022-05-12 16:28:48 +0200

    display project logo and description with flexbox

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import { FiCode, FiExternalLink } from "react-icons/fi";
 const projects = [
   {
     title: "exLibris",
-    logo: "exlibris",
+    img: "exlibris",
     description: "Online book cataloguing tool",
     tags: ["HTML", "CSS", "JavaScript", "React", "Firebase"],
     url: "https://paniszcze.github.io/exlibris/",
@@ -14,7 +14,7 @@ const projects = [
   },
   {
     title: "kībōdo",
-    logo: "kiiboodo",
+    img: "kiiboodo",
     description: "Gamified typing practice of Japanese kana",
     tags: ["HTML", "CSS", "TypeScript", "React"],
     url: "https://paniszcze.github.io/kiiboodo/",
@@ -23,7 +23,7 @@ const projects = [
   },
   {
     title: "Kana Flashcards",
-    logo: "kana_flashcards",
+    img: "kana_flashcards",
     description: "Customizable flashcard deck for learning Japnanese kana",
     tags: ["HTML", "CSS", "JavaScript", "React"],
     url: "https://paniszcze.github.io/kana-flashcards/",
@@ -39,46 +39,64 @@ export default function Projects() {
       <h2>projects</h2>
       <div className="project-list">
         {projects.map(
-          ({ title, logo, description, tags, url, source, theme }, index) => (
-            <article
-              key={index}
-              className="project-item"
-              style={{ backgroundColor: theme.primary, color: theme.text }}
-            >
-              <div className="project-description">
-                <img
-                  src={require("../assets/" + logo + ".svg")}
-                  alt={`${title} logo`}
-                  className="project-logo"
-                />
-                <p>{description}</p>
-              </div>
-              <ul className="tag-list">
-                {tags.map((tag) => (
-                  <li key={tag}>{tag}</li>
-                ))}
-              </ul>
-              <div className="external-links">
-                <a
-                  style={{ color: theme.highlight }}
-                  href={source}
-                  rel="noreferrer"
-                  target="_blank"
-                  alt="view source code"
+          ({ title, img, description, tags, url, source, theme }, index) => (
+            <div className="project-item-wrapper" key={index}>
+              <div
+                className="project-item"
+                style={{
+                  backgroundColor: theme.primary,
+                  color: theme.text,
+                  alignItems: index % 2 === 0 ? "flex-start" : "flex-end",
+                  textAlign: index % 2 === 0 ? "left" : "right",
+                }}
+              >
+                <div className="project-description">
+                  <div
+                    className="project-logo"
+                    style={{
+                      justifyContent:
+                        index % 2 === 0 ? "flex-start" : "flex-end",
+                    }}
+                  >
+                    <img
+                      src={require("../assets/" + img + ".svg")}
+                      alt={`${title} logo`}
+                    />
+                  </div>
+                  <p>{description}</p>
+                </div>
+                <ul
+                  className="tag-list"
+                  style={{
+                    justifyContent: index % 2 === 0 ? "flex-start" : "flex-end",
+                  }}
                 >
-                  <FiCode />
-                </a>
-                <a
-                  style={{ color: theme.highlight }}
-                  href={url}
-                  rel="noreferrer"
-                  target="_blank"
-                  aly="view live demo"
-                >
-                  <FiExternalLink />
-                </a>
+                  {tags.map((tag) => (
+                    <li key={tag}>{tag}</li>
+                  ))}
+                </ul>
+                <div className="external-links">
+                  <a
+                    style={{ color: theme.highlight }}
+                    href={source}
+                    rel="noreferrer"
+                    target="_blank"
+                    alt="view source code"
+                  >
+                    <FiCode />
+                  </a>
+                  <a
+                    style={{ color: theme.highlight }}
+                    href={url}
+                    rel="noreferrer"
+                    target="_blank"
+                    aly="view live demo"
+                  >
+                    <FiExternalLink />
+                  </a>
+                </div>
               </div>
-            </article>
+            </div>
           )
         )}
       </div>
