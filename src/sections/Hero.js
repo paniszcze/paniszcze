2c9e6db5c27596c0a9157ec325d3e13f9e212636
danny <bahuko@gmail.com>
2022-08-09 14:34:08 +0200

    add pixelated portrait in hero, color palette change

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,18 +1,32 @@
-import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { HiOutlineLocationMarker, HiBriefcase } from 'react-icons/hi';
+import HeroImage from '../assets/images/hero.png';
 
 export default function Hero() {
     return (
         <div className="hero">
-            <header>
-                <h1>hi!</h1>
-                <h1>my name is</h1>
-                <h1>daniel panski</h1>
-                <h3>i make things for the web</h3>
-            </header>
-            <div className="info">
-                <div className="location">
-                    <HiOutlineLocationMarker /> Barcelona, Spain
+            <div className="hero-wrapper">
+                <div className="portrait">
+                    <img src={HeroImage} alt="my pixel portrait" />
                 </div>
+                <div className="greeting">
+                    <header>
+                        <h1>
+                            hi! <br />
+                            my name is <br /> daniel panski
+                        </h1>
+                        <h3>i make things for the web</h3>
+                    </header>
+                </div>
+            </div>
+            <div className="info">
+                <ul className="info-list">
+                    <li className="info-item">
+                        <HiOutlineLocationMarker /> Barcelona, Spain
+                    </li>
+                    <li className="info-item">
+                        <HiBriefcase /> ...on the lookout!
+                    </li>
+                </ul>
                 <div className="cta">
                     <a href="#contact" className="btn">
                         contact me
