01e5fb36bec0f153f35213aa2251e8f03146ddbe
danny <bahuko@gmail.com>
2023-06-30 18:43:58 +0200

    update address

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,7 @@ export default function Hero() {
             <div className="info">
                 <ul className="info-list">
                     <li className="info-item">
-                        <HiOutlineLocationMarker /> Gdańsk, Poland
+                        <HiOutlineLocationMarker /> Poland
                     </li>
                     <li className="info-item">
                         <HiBriefcase /> ...on the lookout!
